0d46aaaef0692a03f6f7f22a70d8842bee41dc22
Rutuj <61797632+rutuj24@users.noreply.github.com>
2021-07-18 12:58:03 +0530

    Update settings.py

--- blood_bank/blood_bank/settings.py
+++ blood_bank/blood_bank/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&bo1*=jk$v%ya8v56qfr8l3-jvph1lf@7vg+f&u4f++fjd4+6h'
+SECRET_KEY = 'Secret key of your django project'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
